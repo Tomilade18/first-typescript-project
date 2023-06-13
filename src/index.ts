d9a274db78d272c85970cbb3e1bd84aa503cb3a9
Tomilade18 <jboyyumzey@gmail.com>
2023-06-13 22:43:05 +0100

    added somethings and an event listener in index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,21 @@
 
 // console.log(uuidV4())
 
-const list = document.querySelector("#list")
-const form = document.querySelector("#new-task-form")
-const input = document.querySelector("#new-task-title")
+const list = document.querySelector<HTMLUListElement>("#list")
+const form = document.getElementById("new-task-form") as HTMLFormElement | null
+const input = document.querySelector<HTMLInputElement>("#new-task-title")
+
+
+form?.addEventListener("submit", e => {
+    e.preventDefault()
+
+    if (input?.value == "" || input?.value == null) return
+   
+   const task = {
+    id:
+    title:
+    completed: false,
+    createdAt: new Date()
+   }
+    input.value
+})
