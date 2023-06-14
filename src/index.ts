154abecd1dfaaab7f5b228bca16cef02b018a135
Tomilade18 <jboyyumzey@gmail.com>
2023-06-14 13:58:38 +0100

    added type tasks

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,23 @@
 import {v4 as uuidV4} from 'uuid'
 
-// console.log(uuidV4())
+type Task = {
+    id: string;
+    title: string;
+    completed: boolean;
+    createdAt: Date;
+}
 
 const list = document.querySelector<HTMLUListElement>("#list")
 const form = document.getElementById("new-task-form") as HTMLFormElement | null
 const input = document.querySelector<HTMLInputElement>("#new-task-title")
-
+const tasks: Task[] = []
 
 form?.addEventListener("submit", e => {
     e.preventDefault()
 
     if (input?.value == "" || input?.value == null) return
    
-   const task = {
+   const newTask: Task = {
     id: uuidV4(),
     title: input.value,
     completed: false,
@@ -20,13 +25,16 @@ form?.addEventListener("submit", e => {
    }
     
    addListItem(newTask)
+   input.value = ""
 })
 
-function addListItem(task: {
-    id: string;
-    title: string;
-    completed: boolean;
-    createdAt: Date;
-}) {
-
+function addListItem(task: Task) {
+    const item = document.createElement('li')
+    const label = document.createElement('label')
+    const checkbox = document.createElement('input')
+    checkbox.type = 'checkbox'
+    checkbox.checked = task.completed
+    label.append(checkbox, task.title)
+    item.append(label)
+    list?.append(item)
 }
